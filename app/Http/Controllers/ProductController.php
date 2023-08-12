5cb0442940e16d88864acabe69c8be5b9e789187
Daksh <goyaldaksh5522@gmail.com>
2023-08-12 05:59:24 +0530

    create read delete operation on 12/AUG/2023

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\product;
+use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,56 +11,65 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        return Product::all();
+        }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric|min:0',
+            'quantity' => 'required|integer|min:0',
+        ]);
+
+        $product = Product::create($request->all());
+
+        return response()->json(['product' => $product], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(product $product)
+    public function show(Product $product)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(product $product)
-    {
-        //
+        $product = Product::get();
+        // $product = Product::get(['name']);
+        return response()->json(['product' => $product]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, product $product)
+    public function update(Request $request, Product $product)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric|min:0',
+            'quantity' => 'required|integer|min:0',
+        ]);
+
+        $product->update($request->all());
+
+        return response()->json(['message' => 'Product updated successfully']);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(product $product)
-    {
-        //
+    public function destroy($id)
+    {   
+        // echo $id;
+        $product = Product::where('id',$id);
+        // echo $product;
+        $product->delete();
+        // return Redirect::back()->with('msg', 'The Message');
+        return response()->json(['message' => 'Product deleted successfully']);
     }
 }
